8a83a4917ca3a7107c0c9cf699aa26549cc69341
ericlurker <eric@mystiko.network>
2022-07-05 14:07:36 +0800

    merge with Zokrates 0.7.13, fix zokrates_js build error

--- zokrates_js/src/lib.rs
+++ zokrates_js/src/lib.rs
@@ -278,8 +278,8 @@ mod internal {
     ) -> Result<JsValue, JsValue> {
         let vk: S::VerificationKey =
             serde_json::from_value(vk).map_err(|err| JsValue::from_str(&format!("{}", err)))?;
-
-        Ok(JsValue::from_str(&S::export_solidity_verifier(vk)))
+        let (_, verifier, _) = &S::export_solidity_verifier(vk);
+        Ok(JsValue::from_str(verifier.as_str()))
     }
 }
 
@@ -340,7 +340,6 @@ pub fn export_solidity_verifier(vk: JsValue) -> Result<JsValue, JsValue> {
             JsValue::from_str("Invalid verification key: missing field `scheme`")
         })?)
         .map_err(|e| JsValue::from_str(&e))?;
-
 
     match (curve, scheme) {
         (CurveParameter::Bn128, SchemeParameter::G16) => {
